a42d27fc722a958372030bdca18dd21ea1741933
Sean Venard <seanvenard@gmail.com>
2018-06-25 09:30:32 -0700

    Adds ch10 directory and file related to opening and using files in python.

---
+++ python_crash_course/ch10/file_reader.py
@@ -0,0 +1,9 @@
+with open('pi_digits.txt') as file_object:
+	contents = file_object.read()
+	print(contents.rstrip())
+
+file_name = 'pi_digits.txt'
+
+with open(file_name) as file_object:
+	for line in file_object:
+		print(line.rstrip())
